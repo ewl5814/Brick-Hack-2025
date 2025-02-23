4cec1e023b27dd3e6b3efef7427a2b0e3b32dab1
Chris DeForest <ced6563@rit.edu>
2025-02-23 00:36:07 -0500

    added home button functionality

--- nutRITion.ui/nutrition/src/app/navbar.tsx
+++ nutRITion.ui/nutrition/src/app/navbar.tsx
@@ -1,9 +1,12 @@
 import { Button } from "@/components/ui/button"
 import { HandPlatter, HomeIcon, UserIcon } from "lucide-react"
+import { useRouter } from "next/navigation";
+
 export default function Nav() {
+    const router = useRouter();
     return(
         <div className="w-full h-8 justify-center m-2 flex flex-row gap-2">
-            <Button variant="ghost">
+            <Button variant="ghost" onClick={() => router.push("/welcome")}>
                 <HomeIcon />
                 <p>Home</p>
             </Button>
